f585b8dab6b5d9a62e6033c927d0592d5f5ec8ce
Ben Orrin <114030615+benorrin@users.noreply.github.com>
2024-08-15 18:27:22 +0100

    Improved Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-"use client";  // Mark this component as a Client Component
+"use client";
 
 import React, { useState } from 'react';
 import { AppBar, Container, Toolbar, Typography, Button, IconButton, Box, Menu, MenuItem } from '@mui/material';
@@ -22,7 +22,7 @@ const Navbar: React.FC = () => {
       <Container maxWidth="lg">
         <Toolbar disableGutters>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            {/* Logo linking to the homepage */}
+            {/* Site Logo */}
             <a href={rootDomain} style={{ textDecoration: 'none', color: 'inherit' }}>
               <Box component="span" sx={{ fontWeight: 'bold', fontSize: '1.5rem', mr: 1 }}>
                 <Box component="span" sx={{ color: 'blue' }}>Brighton</Box>
@@ -49,31 +49,29 @@ const Navbar: React.FC = () => {
               sx={{
                 display: { xs: 'block', md: 'none' },
                 '& .MuiPaper-root': {
-                  width: '80vw', // Full width of the viewport
-                  maxWidth: '80vw', // Remove max width constraint
-                  right: 0, // Align with the left edge
-                  top: '56px !important', // Adjust top position to align with AppBar
-                  borderRadius: 0, // Remove border-radius
+                  width: '80vw',
+                  maxWidth: '80vw',
+                  right: 0,
+                  top: '56px !important',
+                  borderRadius: 0,
                 },
               }}
             >
-              <MenuItem onClick={handleMenuClose}><a href={`${rootDomain}/landings`}>Landings</a></MenuItem>
-              <MenuItem onClick={handleMenuClose}><a href={`${rootDomain}/company`}>Company</a></MenuItem>
-              <MenuItem onClick={handleMenuClose}><a href={`${rootDomain}/account`}>Account</a></MenuItem>
-              <MenuItem onClick={handleMenuClose}><a href={`${rootDomain}/pages`}>Pages</a></MenuItem>
+              <MenuItem onClick={handleMenuClose}><a href={`${rootDomain}/eat`}>Eat</a></MenuItem>
+              <MenuItem onClick={handleMenuClose}><a href={`${rootDomain}/drink`}>Drink</a></MenuItem>
+              <MenuItem onClick={handleMenuClose}><a href={`${rootDomain}/events`}>What's on</a></MenuItem>
               <MenuItem onClick={handleMenuClose}><a href={`${rootDomain}/blog`}>Blog</a></MenuItem>
-              <MenuItem onClick={handleMenuClose}><a href={`${rootDomain}/portfolio`}>Portfolio</a></MenuItem>
+			  <MenuItem onClick={handleMenuClose}><a href={`${rootDomain}/about`}>About</a></MenuItem>
             </Menu>
           </Box>
 
           {/* Links for desktop view */}
           <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: 3 }}>
-            <Button color="inherit" href={`${rootDomain}/landings`}>Landings</Button>
-            <Button color="inherit" href={`${rootDomain}/company`}>Company</Button>
-            <Button color="inherit" href={`${rootDomain}/account`}>Account</Button>
-            <Button color="inherit" href={`${rootDomain}/pages`}>Pages</Button>
+            <Button color="inherit" href={`${rootDomain}/eat`}>Eat</Button>
+            <Button color="inherit" href={`${rootDomain}/drink`}>Drink</Button>
+            <Button color="inherit" href={`${rootDomain}/events`}>What's on</Button>
             <Button color="inherit" href={`${rootDomain}/blog`}>Blog</Button>
-            <Button color="inherit" href={`${rootDomain}/portfolio`}>Portfolio</Button>
+			<Button color="inherit" href={`${rootDomain}/about`}>About</Button>
           </Box>
         </Toolbar>
       </Container>
